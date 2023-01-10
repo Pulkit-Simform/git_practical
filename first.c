e5a14629e86db3a55896506775e028e39ad7976d
Pulkit Sharma <pulkit@simformsolutions.com>
2023-01-10 12:37:34 +0530

    [dropped previous commit]1st commit that will not be dropped

--- first.c
+++ first.c
@@ -4,6 +4,9 @@ void cherry_pick_1_method(){
     printf("Cherry_pick_1 method");
 }
 
+void drop_2_commit(){
+    printf("This commit will not be droppped");
+}
 void solve(){
     printf("From solve method");
 }
